50a86dbba37a24d93069d3314cfe0a68dd5074b5
Anthony Sibaja <sibajagranados@gmail.com>
2024-01-29 16:56:59 -0600

    parte 2 final

---
+++ middleware/app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        //request->session()->get('tony');
+        //echo $request->session()->all();
+
+        // session(['tony'=>'master_instructor']);
+        // echo session('tony1');
+
+       // return view('home');
+        //$request->session()->forget('tony1');
+        $request->session()->flush();
+       return $request->session()->all();
+
+    }
+}
