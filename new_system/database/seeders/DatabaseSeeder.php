b169958720cfceb673c81355cf2f704eacef33df
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-02 13:52:13 -0600

    video 194
    se configuran los modelos post y user para la relación también se crear un factory y se configura, se tambien se trabaja con las semillas y se configuran y se añaden datos a las tablas

--- new_system/database/seeders/DatabaseSeeder.php
+++ new_system/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Seed the application's database.
@@ -20,5 +23,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory(10)->create()->each(function ($user) {
+            $user->posts()->save(Factory::factoryForModel('App\Models\Post')->make());
+        });
+
     }
 }
