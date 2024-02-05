c59f5cd28f5681aa091a747e15715c03c2e0a3f4
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-05 10:18:59 -0600

    video 201
    se configura una tabla en la vista que creamos anteriormente para poder visualizar los datos, se configuran los js para la tabla y una mejor visualización

--- new_system/resources/views/components/admin-master.blade.php
+++ new_system/resources/views/components/admin-master.blade.php
@@ -378,8 +378,8 @@
   <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
-
+  <script src="{{asset('js/sb-admin-2.min.js')}}"></script> 
+  @yield('scripts')
 </body>
 
 </html>
